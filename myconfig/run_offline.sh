619371226127ef70eb7bc061c108617b3d74fdd4
Kapiyva <ri9.py1201@gmail.com>
2020-10-10 23:50:54 +0900

    videoをignore、相対パスで指定

--- myconfig/run_offline.sh
+++ myconfig/run_offline.sh
@@ -1,2 +1,2 @@
-../build/run_video_slam  -v orb_vocab.dbow2 -c sample.yaml -m ~/Videos/video1.mp4 -n ~/Videos/video2.mp4 
+../build/run_video_slam  -v orb_vocab.dbow2 -c sample.yaml -m ./video1.mp4 -n ./video2.mp4 
 
